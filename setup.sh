1f614217023193e94216f9a0d6240ecb3e78ec06
bhuism <bhuism@odee.net>
2020-10-31 13:04:19 +0100

    add installer

--- setup.sh
+++ setup.sh
@@ -5,3 +5,18 @@ if [ `id -u` != "0" ] ; then
    exit 100
 fi
 
+#LOG_FILE=/tmp/$0.log
+
+#rm -Rf ${LOG_FILE}
+#touch LOGFILE;
+
+#exec 1<&- 2<&- 1<>${LOG_FILE} 2>&1
+#exec 1<>${LOG_FILE} 2>&1
+
+curl -s https://raw.githubusercontent.com/bhuism/webcam/master/stream_camera.service -o /etc/systemd/system/stream_camera.service
+
+systemctl daemon-reload
+
+systemctl enable systemd-time-wait-sync
+
+systemctl status stream_camera
