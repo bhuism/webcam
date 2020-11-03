81c1733fe13278ed3207fa7c604c745dd4de9dc3
bhuism <bhuism@odee.net>
2020-11-03 23:24:35 +0100

    cleanups

--- setup.sh
+++ setup.sh
@@ -1,34 +1,34 @@
 #!/bin/bash
 
-if [ `id -u` != "0" ] ; then
-   echo "You must be root to do this." 1>&2
-   exit 100
+if [ $(id -u) != "0" ]; then
+  echo "You must be root to do this." 1>&2
+  exit 100
 fi
 
-if [ ${HOSTTYPE} != "arm" ] ; then
-   echo No arm architecture 1>&2
-   exit 101
+if [ ${HOSTTYPE} != "arm" ]; then
+  echo No arm architecture 1>&2
+  exit 101
 fi
 
-if [ ! -c /dev/video0 ] ; then
-    echo "No camera device /dev/video0" 1>&2
-    exit 102
+if [ ! -c /dev/video0 ]; then
+  echo "No camera device /dev/video0" 1>&2
+  exit 102
 fi
 
 CURL=(-s -H "Cache-Control: no-cache")
 
 echo stopping watchdog
 
-systemctl stop watchdog &> /dev/null
+systemctl stop watchdog &>/dev/null
 
 echo stopping stream_camera
 
-systemctl stop stream_camera &> /dev/null
+systemctl stop stream_camera &>/dev/null
 
 #echo updating apt
 #apt update
 
-echo installing cam.sh 
+echo installing cam.sh
 
 curl "${CURL[@]}" https://raw.githubusercontent.com/bhuism/webcam/master/cam.sh -o /home/pi/cam.sh
 chown pi:pi /home/pi/cam.sh
@@ -59,17 +59,17 @@ echo installing watchdog.conf
 
 curl "${CURL[@]}" https://raw.githubusercontent.com/bhuism/webcam/master/watchdog.conf -o /etc/watchdog.conf
 
-systemctl restart watchdog
-
 echo installing htmls
 
 HTMLDIR=/var/www/html
 
-curl "${CURL[@]}"  https://raw.githubusercontent.com/bhuism/webcam/master/index.html -o ${HTMLDIR}/index.html \
-	https://raw.githubusercontent.com/bhuism/webcam/master/style.css -o ${HTMLDIR}/style.css
+curl "${CURL[@]}" https://raw.githubusercontent.com/bhuism/webcam/master/index.html -o ${HTMLDIR}/index.html \
+  https://raw.githubusercontent.com/bhuism/webcam/master/style.css -o ${HTMLDIR}/style.css
 
 ln -sf /dev/shm/streaming /var/www/html/
 
 echo starting stream_camera
 
 systemctl start stream_camera
+
+systemctl restart watchdog
