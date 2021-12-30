e1be68dd153902e5af2912260efaa5b5886af186
Bas Huisman <bhuism@users.noreply.github.com>
2021-12-30 22:43:51 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -53,7 +53,7 @@ systemctl enable stream_camera
 
 echo install ffmpeg watchdog nginx
 
-apt install -y ffmpeg watchdog nginx unattended-upgrades 
+apt install -y ffmpeg watchdog nginx unattended-upgrades ntp
 
 echo installing watchdog.conf
 
