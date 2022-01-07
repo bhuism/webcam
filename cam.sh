e86de766182bf66866c9099039ca891a50976cff
Bas Huisman <bhuism@users.noreply.github.com>
2022-01-07 21:28:52 +0100

    Update cam.sh

--- cam.sh
+++ cam.sh
@@ -2,32 +2,39 @@
 
 DIR=/dev/shm/streaming
 RESOLUTION=hd720
-FRAMERATE=24
+FRAMERATE=25
 GOP=$(($FRAMERATE * 2))
-FILENAME=stream-me-baby
 
 #INPUTFORMAT=h264
-INPUTFORMAT=yuv420p
+#INPUTFORMAT=yuv420p
 
 #ENCODER=copy
 ENCODER=h264_omx
 
-BITRATE=3M
+BITRATE=2M
 
 trap "rm -Rf $DIR" EXIT
 
 rm -Rf $DIR
 mkdir $DIR
 
-ffmpeg -nostdin -hide_banner -loglevel warning -y \
-  -f video4linux2 -input_format ${INPUTFORMAT} -video_size ${RESOLUTION} -framerate ${FRAMERATE} -i /dev/video0 \
+#  -vf "settb=AVTB,setpts='trunc(PTS/1K)*1K+st(1,trunc(RTCTIME/1K))-1K*trunc(ld(1)/1K)' \
+#		,drawtext=text='%{localtime}.%{eif\:1M*t-1K*trunc(t*1K)\:d\:3}:fontsize=20:fontcolor=wheat:x=(w-tw)/2:y=16'" \
+
+
+# ffmpeg -r 25 -i /dev/video0  -vcodec copy -acodec aac -hls_time 5 -hls_list_size 5 -hls_flags delete_segments /tmp/temp/stream.m3u8
+
+# ffmpeg -r 25 -f video4linux2 -input_format h264 -i /dev/video0 -video_size hd720 -vcodec copy -acodec aac -hls_list_size 5 -hls_flags delete_segments /dev/shm/streaming/master.m3u8
+
+
+nice -n -10 ffmpeg -nostdin -y -loglevel warning \
+  -f video4linux2 -framerate ${FRAMERATE} -video_size ${RESOLUTION} -i /dev/video0 \
   -vf "settb=AVTB,setpts='trunc(PTS/1K)*1K+st(1,trunc(RTCTIME/1K))-1K*trunc(ld(1)/1K)' \
-		,drawtext=text='%{localtime}.%{eif\:1M*t-1K*trunc(t*1K)\:d\:3}:fontsize=20:fontcolor=wheat:x=(w-tw)/2:y=16'" \
-  -b:v ${BITRATE} -maxrate:v ${BITRATE} -bufsize ${BITRATE} -g ${GOP} -profile:v high \
-  -c:v ${ENCODER} \
+	,drawtext=text='%{localtime}.%{eif\:1M*t-1K*trunc(t*1K)\:d\:3}:fontsize=20:fontcolor=wheat:x=(w-tw)/2:y=16'" \
+  -b:v ${BITRATE} -maxrate:v ${BITRATE} -bufsize ${BITRATE} -g ${GOP} -profile:v high -c:v ${ENCODER} \
   -f hls \
   -hls_flags delete_segments+temp_file+independent_segments \
-  -hls_allow_cache 0 \
+  -hls_list_size 3 \
   -hls_segment_type fmp4 \
   -hls_start_number_source datetime \
   $DIR/master.m3u8
